f229b60598f8e1b3fae16d0734b28c57645a9882
hieudt <hieudt10092019@gmail.com>
2020-10-22 23:00:24 +0700

    create model

---
+++ eShopSolution.Data/EF/eShopDataContext.cs
@@ -0,0 +1,28 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopDataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public eShopDataContext(DbContextOptions<eShopDataContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        
+    }
+}
