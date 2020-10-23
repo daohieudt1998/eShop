9cd94c5f6a02106a92db5a052ed3cdc272e239ae
hieudt <hieudt10092019@gmail.com>
2020-10-23 23:14:00 +0700

    learn configuration

---
+++ eShopSolution.Data/Configurations/OrderDetailConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class OrderDetailConfiguration: IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.Order)
+                .WithMany(y => y.OrderDetails)
+                .HasForeignKey(z => z.OrderId);
+
+            builder.HasOne(x => x.Product)
+                .WithMany(y => y.OrderDetails)
+                .HasForeignKey(z => z.ProductId);
+        }
+    }
+}
---
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguartion.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ProductInCategoryConfiguartion : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.HasKey(x => new { x.CategoryId, x.ProductId });
+
+            builder.HasOne(x => x.Product)
+                .WithMany(y => y.ProductInCategories)
+                .HasForeignKey(z => z.ProductId);
+
+            builder.HasOne(x => x.Category)
+                .WithMany(y => y.ProductInCategories)
+                .HasForeignKey(z => z.CategoryId);
+        }
+    }
+}
